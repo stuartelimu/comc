9ad90320dd950fa5765c4ef9022fa4998faaf136
ElimuStuart <stuartelimu@gmail.com>
2019-12-14 03:19:47 +0300

    created Header component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <div className="header">
+            <h2>COMC</h2>
+            <p>We deal in coms...</p>
+        </div>
+    )
+}
+
+export default Header;
