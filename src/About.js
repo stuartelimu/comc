f4e9626d46982922a7dbe6f3f6c5d45fd157da94
ElimuStuart <stuartelimu@gmail.com>
2019-12-14 03:31:15 +0300

    create About component

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="section bg-grey">
+      <div className="small-div"></div>
+      <div className="big-div"></div>
+    </div>
+  );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import About from './About';
 
 function App() {
   return (
-    <>
+    <div className="body">
       <Header />
-      <div>2</div>
+      <About />
       <div>3</div>
       <div>4</div>
       <div>5</div>
-    </>
+    </div>
   );
 }
 
